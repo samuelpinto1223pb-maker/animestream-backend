cabb657d6c9fc476af5a30d761612630ac601a6f
samuelpinto1223pb-maker <samuelpinto1223pb@gmail.com>
2026-08-29 15:05:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Servir archivos estáticos (index.html, latino.json, imágenes, etc.)
+// Servir automáticamente todos los archivos estáticos de la raíz (index.html, latino.json, etc.)
 app.use(express.static(path.join(__dirname, './')));
 
-// Ruta principal para cargar la web
+// Ruta principal que entrega el archivo de la interfaz
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Ruta API para obtener los datos si se requieren directamente
+// Ruta API por si quieres consultar los datos directamente
 app.get('/api/anime', (req, res) => {
   res.sendFile(path.join(__dirname, 'latino.json'));
 });
